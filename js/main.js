60eb7abb51bc61daf96155b1c78fb91169a95502
Ravencer <ravencer880@gmail.com>
2020-11-25 22:56:04 +0700

    fixed: api js

--- js/main.js
+++ js/main.js
@@ -9,24 +9,3 @@ var mySwiper = new Swiper('.swiper-container', {
     onlyInViewport: false,
   }
 })
-
-ymaps.ready(init);
-function init(){
-        
- var myMap = new ymaps.Map("map", {
-            
-  center: [7.8381095967729015,98.29892085940638],
-            
-            
-  zoom: 17
-});
-var myGeoObject = new ymaps.GeoObject({
-    geometry: {
-        type: "Point", 
-        coordinates: [7.83810825941045,98.29888599068919] 
-              }
-        });
-
-// Размещение геообъекта на карте.
-myMap.geoObjects.add(myGeoObject); 
-    }
