b1a9bdab6806ca953466b1c5b59fda5f40ad498d
Ravencer <ravencer880@gmail.com>
2020-12-07 19:51:06 +0700

    create: esc close dialog window

--- js/main.js
+++ js/main.js
@@ -55,6 +55,14 @@ $(document).ready(function(){
       var closeModalButton = $(".modal__close");
       modalButton.on('click', openModal);
       closeModalButton.on('click', closeModal);
+      $(document).on('keydown', function(e) {
+        if (e.keyCode == 27){
+          var modalOverlay = $(".modal__overlay");
+          var modalDialog = $(".modal__dialog");
+          modalOverlay.removeClass('modal__overlay--visible');
+          modalDialog.removeClass('modal__dialog--visible');
+        }
+      });
 
       function openModal (){
         var modalOverlay = $(".modal__overlay");
@@ -70,4 +78,5 @@ $(document).ready(function(){
         modalOverlay.removeClass('modal__overlay--visible');
         modalDialog.removeClass('modal__dialog--visible');
       }
+      
 });
