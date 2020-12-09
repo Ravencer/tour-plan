ee11b792e68dfb58dfbe71fabf2f894246a1522d
Ravencer <ravencer880@gmail.com>
2020-12-09 19:36:51 +0700

    fixed: js and html

--- js/main.js
+++ js/main.js
@@ -25,32 +25,6 @@ $(document).ready(function(){
   menuButton.addEventListener('click', function(){
     document.querySelector(".navbar-bottom").classList.toggle('navbar-bottom--visible');
   });
-  // Функция ymaps.ready() будет вызвана, когда
-      // загрузятся все компоненты API, а также когда будет готово DOM-дерево.
-      ymaps.ready(init);
-      function init(){
-          // Создание карты.
-          var myMap = new ymaps.Map("map", {
-              // Координаты центра карты.
-              // Порядок по умолчанию: «широта, долгота».
-              // Чтобы не определять координаты центра карты вручную,
-              // воспользуйтесь инструментом Определение координат.
-              center: [7.8381095967729015,98.29892085940638],
-              
-              // Уровень масштабирования. Допустимые значения:
-              // от 0 (весь мир) до 19.
-              zoom: 17
-          });
-          var myGeoObject = new ymaps.GeoObject({
-      geometry: {
-          type: "Point", // тип геометрии - точка
-          coordinates: [7.83810825941045,98.29888599068919] // координаты точки
-                }
-          });
-  
-  // Размещение геообъекта на карте.
-  myMap.geoObjects.add(myGeoObject); 
-      }
       var modalButton = $('[data-toggle="modal"]');
       var closeModalButton = $(".modal__close");
       modalButton.on('click', openModal);
