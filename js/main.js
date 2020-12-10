e59caa45820b3f99129fd9ce7ee505fa9e28ab47
Ravencer <ravencer880@gmail.com>
2020-12-11 05:35:38 +0700

    create: lazy loading

--- js/main.js
+++ js/main.js
@@ -84,7 +84,15 @@ $(document).ready(function(){
           }
         }
       });
+      	
+      
       AOS.init();
+      [].forEach.call(document.querySelectorAll('img[data-src]'),    function(img) {
+        img.setAttribute('src', img.getAttribute('data-src'));
+        img.onload = function() {
+          img.removeAttribute('data-src');
+        };
+      });
 });
 
       
